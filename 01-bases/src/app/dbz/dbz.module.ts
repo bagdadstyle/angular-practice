c54df95845c70e46144e1eb76f47cbd830ac38e1
bagdadstyle <mau20410@gmail.com>
2023-01-01 23:45:34 -0300

    add exercise

--- 01-bases/src/app/dbz/dbz.module.ts
+++ 01-bases/src/app/dbz/dbz.module.ts
@@ -5,10 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
 import { CharactersComponent } from './characters/characters.component';
 import { AddCharComponent } from './add-char/add-char.component';
+import { DbzService } from './services/dbz.service';
 
 @NgModule({
   declarations: [MainPageComponent, CharactersComponent, AddCharComponent],
   exports: [MainPageComponent],
   imports: [CommonModule, FormsModule],
+  providers: [
+    DbzService
+  ]
 })
 export class DbzModule {}
--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,25 +8,12 @@ import { Character } from '../interfaces/dbz.interface';
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
-  characters: Character[] = [
-    {
-      name: 'Gokú',
-      power: 15000,
-    },
-    {
-      name: 'Vegeta',
-      power: 7500,
-    },
-  ];
-
   newChar: Character = {
-    name: '',
-    power: 0,
+    name: 'Character1',
+    power: 1000,
   };
-  
+   
 
   
-  // deleteChar(): void {
-  //   this.characters.pop();
-  // }
+  constructor() {}
 }
